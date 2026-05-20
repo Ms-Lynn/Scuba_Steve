848f41fcfad5fe3927f0ca6c151657e39f927ea1
Ms. Lynn <117663038+Ms-Lynn@users.noreply.github.com>
2026-05-20 15:03:44 -0600

    2.2 Added class modules

---
+++ score.py
@@ -0,0 +1,13 @@
+highScoreFile = "SplunkingHighScore.txt"
+highScore = 0     
+
+
+def getHighScore():
+    global highScore
+    with open(highScoreFile, 'r') as file:
+        highScore = file.read()
+
+
+def newHighScore(newScore):
+    with open(highScoreFile, 'w') as file:
+        file.write(newScore)
